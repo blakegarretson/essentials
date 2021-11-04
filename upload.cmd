01ba5073222ba04d5f2697c57996b8e6b8688c3e
Blake Garretson <blake.garretson@gmail.com>
2021-11-04 18:10:11 -0400

    x

--- upload.cmd
+++ upload.cmd
@@ -1,1 +1,1 @@
-scp about.html changelog.html index.html rejects.html main.css blakegarretson@blakesessentials.com:/home/blakegarretson/blakesessentials.com/
+scp -i "C:\Users\garretson\Google Drive\Backup Data\sshkeys\blakesessentials\be.id_rsa" about.html changelog.html index.html rejects.html main.css blakegarretson@blakesessentials.com:/home/blakegarretson/blakesessentials.com/
